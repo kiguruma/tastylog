7f6c535eec449010fbd7ac5a06f50f5a3e8e60d4
ryosuke yamoto <ryosukeyamoto@ryosukenoMacBook-Pro.local>
2022-03-19 06:44:47 +0900

    react+exppress繋ぎこみ

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const PORT = process.env.PORT
-console.log(process.env)
+// const PORT = process.env.PORT
+// console.log(process.env)
 
+// const express = require("express")
+// const app = express()
+
+// app.get("/",(req,res)=>{
+//     res.end("hello world")
+// })
+
+// app.listen(PORT,()=>{
+//     console.log("サーバー起動")
+//     console.log(PORT)
+// })
+// import express from 'express';
 const express = require("express")
-const app = express()
+// import ssr from './src/ssr';
+const ssr = require("./src/ssr")
 
-app.get("/",(req,res)=>{
-    res.end("hello world")
-})
+const app = express();
 
+// 3000番ポートでWebサーバを立てる
 app.listen(PORT,()=>{
-    console.log("サーバー起動")
-    console.log(PORT)
-})
+       console.log("サーバー起動")
+       console.log(PORT)
+      })
+// https://localhost:3000 にアクセスがあったら ssr() を返す
+app.get('/', (_, res) => {
+  res.send(ssr());
+  console.log("ee")
+});
